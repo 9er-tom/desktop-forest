2a0adfe2c7165c5b136cff5e3a513b5ac3e7013b
Tomas Neuner <tomas.neuner@acds.at>
2025-08-11 12:47:10 +0200

    spawn slot size based on largest plant texture

--- scripts/groundSpawner.gd
+++ scripts/groundSpawner.gd
@@ -5,16 +5,31 @@ extends Node
 @export var spawnButton: TextureButton
 @export var spawnSlotPrefab: PackedScene
 @export var cursorTool: CursorTool
-@export var gridCellSize: Vector2i
+@export var gridCellSize: Vector2
 @export var gridMaxGutter := 20
-@export var gridCellYMaxOffset : int
-@export var gridCellYMinOffset : int
+@export var gridCellYMaxOffset: int
+@export var gridCellYMinOffset: int
 
-var groundPosY : float
-var usedSlots : Array[String]
+var groundPosY: float
+var usedSlots: Array[String]
+
 
 func _ready() -> void:
 	_make_spawn_grid.call_deferred() # defer this to after main window resize
+	_calc_grid_cell_size()
+
+
+## Spawn an instance of every plant prefab and get the size of the biggest plant. 
+## Use this size as gridCellSize for spawning slots.
+## Afterwards, remove all spawned instances	
+
+func _calc_grid_cell_size() -> void:
+	for prefab in plantPrefabs:
+		var plant := prefab.instantiate()
+		add_child(plant)
+		if plant.size > gridCellSize:
+			gridCellSize = plant.size
+		remove_child(plant)
 
 
 func _make_spawn_grid() -> void:
@@ -27,17 +42,16 @@ func _make_spawn_grid() -> void:
 		slot.position = Vector2(currentGridX, groundPosY - gridCellSize.y - randi_range(gridCellYMinOffset, gridCellYMaxOffset))
 		slot.size = gridCellSize
 		add_child(slot)
-		print_debug(slot.position)
 		currentGridX += gridCellSize.x + gutter
 
 
 func spawn_in_slot(slot: SpawnSlot)-> void:
 	if usedSlots.find(slot.name) != -1:
 		return
-		
+
 	var plantIndex := randi_range(0, plantPrefabs.size() - 1)
 	var plantNode  := plantPrefabs[plantIndex].instantiate()
-	
+
 	get_tree().root.add_child(plantNode)
 	usedSlots.append(slot.name)
 	plantNode.position = slot.position
